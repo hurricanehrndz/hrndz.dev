4c48d85ede8fd68f36bdd58a3c2def0c3d962e5a
Carlos Hernandez <carlos@hrndz.ca>
2024-11-06 20:39:23 -0700

    Add initial script to sync zet to content

--- flake/packages/build-content/script.sh
+++ flake/packages/build-content/script.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-echo "hello world"
-# if [[ ! -d "./zet" ]]; then
-#     git clone --depth 1 git@github.com:hurricanehrndz/zet.git zet
-# fi
+current_dir="$PWD"
+zet_dir="${current_dir}/zet"
+posts_src="${zet_dir}/posts/"
+notes_src="${zet_dir}/notes/"
+posts_dst="${current_dir}/content/posts/"
+notes_dst="${current_dir}/content/notes/"
+
+if [[ ! -d "${zet_dir}" ]]; then
+    git clone --depth 1 git@github.com:hurricanehrndz/zet.git zet
+else
+    pushd "${zet_dir}" || exit 1
+    git pull
+    popd || exit 1
+fi
+
+if [[ ! -d "${posts_src}" ]] || [[ ! -d "${notes_src}" ]]; then
+    printf "\e[0;31mSource directories missing\e[0m\n"
+fi
+
+rsync --recursive --delete --exclude '.gitkeep' "${posts_src}" "${posts_dst}"
+rsync --recursive --delete --exclude '.gitkeep' "${notes_src}" "${notes_dst}"
+
+printf "\e[1;92mContent built from zet successfully\e[0m\n"
