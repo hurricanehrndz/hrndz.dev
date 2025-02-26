dbfb60bbfb89d06615d9cfa6c7fe04ebcd9770f7
Carlos Hernandez <carlos@hrndz.ca>
2025-02-26 09:15:20 -0700

    Support showing secure notes locally

--- flake/packages/build-content/script.sh
+++ flake/packages/build-content/script.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 current_dir="$PWD"
-zet_dir="${current_dir}/zet"
+zet_dir=${ZET_DIR:-"${current_dir}/zet"}
 posts_src="${zet_dir}/posts/"
 notes_src="${zet_dir}/notes/"
+snotes_src="${zet_dir}/snotes/"
 posts_dst="${current_dir}/content/posts/"
 notes_dst="${current_dir}/content/notes/"
+snotes_dst="${current_dir}/content/snotes/"
 
 if [[ ! -d "${zet_dir}" ]]; then
     git clone --depth 1 git@github.com:hurricanehrndz/zet.git zet
@@ -21,6 +23,10 @@ fi
 
 rsync --recursive --delete --exclude '.gitkeep' "${posts_src}" "${posts_dst}"
 rsync --recursive --delete --exclude '.gitkeep' "${notes_src}" "${notes_dst}"
+# not running on github
+if [[ ${CI} != 1 ]]; then
+    rsync --recursive --delete --exclude '.gitkeep' "${snotes_src}" "${snotes_dst}"
+fi
 
 git checkout -- content
 
