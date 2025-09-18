b1813a2975d30c993983f4ef5c9c2318296f4517
Carlos Hernandez <carlos@hrndz.ca>
2025-09-17 18:21:22 -0600

    Show snotes locally

--- flake/packages/build-content/script.sh
+++ flake/packages/build-content/script.sh
@@ -4,8 +4,14 @@ current_dir="$PWD"
 zet_dir="${current_dir}/zet"
 posts_src="${zet_dir}/posts/"
 notes_src="${zet_dir}/notes/"
+snotes_src="${zet_dir}/snotes/"
 posts_dst="${current_dir}/content/posts/"
 notes_dst="${current_dir}/content/notes/"
+
+if [[ ${CI:-0} == 0 ]]; then
+    rm -rf zet
+    ln -sf "${HOME}/zet" zet
+fi
 
 if [[ ! -d "${zet_dir}" ]]; then
     git clone --depth 1 git@github.com:hurricanehrndz/zet.git zet
@@ -21,6 +27,9 @@ fi
 
 rsync --recursive --delete --exclude '.gitkeep' "${posts_src}" "${posts_dst}"
 rsync --recursive --delete --exclude '.gitkeep' "${notes_src}" "${notes_dst}"
+if [[ ${CI:-0} == 0 ]]; then
+    rsync --recursive --exclude '.gitkeep' "${snotes_src}/" "${notes_dst}/"
+fi
 
 git checkout -- content
 
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/hurricanehrndz/hrndz.dev
 
 go 1.21.3
 
-require github.com/jpanther/congo/v2 v2.9.0 // indirect
+require github.com/jpanther/congo/v2 v2.11.0 // indirect
